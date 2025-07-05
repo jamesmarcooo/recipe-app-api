bf1f27016d255e887246bc7a7475028bb6cbe166
jamesmarcooo <reyes.jamesmarco03@gmail.com>
2025-07-05 18:03:26 +0800

    Build ingredients API

--- app/core/models.py
+++ app/core/models.py
@@ -57,7 +57,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
-    Ingredient = models.ManyToManyField('Ingredient')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
@@ -76,8 +76,12 @@ class Tag(models.Model):
 
 
 class Ingredient(models.Model):
+    """Ingredient for recipes."""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.name
